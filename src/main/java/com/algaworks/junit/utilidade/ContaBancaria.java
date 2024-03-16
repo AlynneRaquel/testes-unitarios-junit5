cda9ad2bf827bac86a6c681a4fa221208c8f063d
AlynneRaquel <alynneraquel@hotmail.com>
2024-03-16 11:29:36 -0300

    Refactor - Desafio - Implementando conta bancaria e teste de conta bancária 2

--- src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -1,5 +1,6 @@
 package com.algaworks.junit.utilidade;
 
+import java.lang.constant.MethodHandleDesc;
 import java.math.BigDecimal;
 
 public class ContaBancaria {
@@ -7,7 +8,7 @@ public class ContaBancaria {
     private BigDecimal saldo;
 
     public ContaBancaria(BigDecimal saldo) {
-        if (saldo == null){
+        if (saldo == null) {
             throw new IllegalArgumentException("Valor inválido");
         }
 
@@ -15,19 +16,23 @@ public class ContaBancaria {
     }
 
     public void saque(BigDecimal valor) {
-        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0  ) {
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Valor inválido ou nulo");
         }
 
-        this.saldo =  saldo().subtract(valor);
+        if (this.saldo.compareTo(valor) < 0) {
+            throw new RuntimeException("Saldo Insuficiente");
+        }
+
+        this.saldo = saldo().subtract(valor);
     }
 
     public void deposito(BigDecimal valor) {
-        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0  ) {
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Valor inválido ou nulo");
         }
 
-        this.saldo =  saldo().add(valor);
+        this.saldo = saldo().add(valor);
     }
 
     public BigDecimal saldo() {
